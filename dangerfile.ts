19643b4109482d5d5ad1c8086182990e71abb538
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-01-02 15:04:18 -0300

    remove the rule to length of pr body

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,6 +3,6 @@ import { message, danger, fail } from "danger";
 const modifiedMD = danger.git.modified_files.join("- ");
 message("Changed Files in this PR: \n - " + modifiedMD);
 
-if (danger.github.pr.body && danger.github.pr.body.length < 10) { 
+if (danger.github.pr.body) { 
   fail("This pull request needs a description")
 }
